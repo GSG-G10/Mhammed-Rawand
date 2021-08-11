5436f5353cdcaabd730e0fb815aac0f20d683736
rawand1998 <rawandgaradh@gmail.com>
2021-08-11 13:05:20 +0300

    render in index page

--- public/dom.js
+++ public/dom.js
@@ -2,33 +2,19 @@ const searchInput = document.getElementById("searchInput");
 const btnSearch = document.querySelector('#btn-search');
  const content =document.querySelector('.content');
 
- const showImage = (results)=>{
-  const result =results.urls.raw;
-  for(let i =0 ; i<10 ;i++){
-  const image = document.createElement('img')
-  age.src= result[i].urls.raw
-  content.appendChild('image')
-}
-
+ const showImage = (response)=>{
+   const image = document.createElement('img')
+   image.src= response.results[1].urls.small
+   content.appendChild(image)
 }
 
 btnSearch.addEventListener('click',()=>{
- // e.preventDefault();
-  fetch(`/input?query=${searchInput.value}`)
+  fetch(`/input/${searchInput.value}`)
   .then((response) => response.json())
- .then(showImage)
- .catch((err)=>console.log(err))
-    });
+  .then(showImage)
+  
+    })
 
-// input.oninput = () => {
-//   console.log("in search");
-//   console.log("input", input.value);
-//   const value = input.value;
-//   fetch(`/search?query=${value}`)
-//     .then((response) => response.json())
-//     .then((response) => {
-//       // const resultsDiv = document.getElementById("");
-//       // resultsDiv.appendChild()
-//       console.log("response", response);
-//     });
-// };
+
+  
+
