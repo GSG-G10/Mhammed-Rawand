cf546c7fb6356d120fe83aee7f573194d5195cb6
rawand1998 <rawandgaradh@gmail.com>
2021-08-11 15:59:12 +0300

    home page

--- public/dom.js
+++ public/dom.js
@@ -2,9 +2,12 @@ const searchInput = document.getElementById("searchInput");
 const btnSearch = document.querySelector('#btn-search');
  const content =document.querySelector('.content');
 
+
  const showImage = (response)=>{
+   content.innerHTML=''
    const image = document.createElement('img')
    image.src= response.results[1].urls.small
+   image.classList='img'
    content.appendChild(image)
 }
 
